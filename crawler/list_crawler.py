5012d466032b1215429343741fec59c679b57f17
HumanIearning <kimdh3896@naver.com>
2024-03-09 23:55:43 +0900

    feat: crawl all top 90 goods url in list page of one category

--- crawler/list_crawler.py
+++ crawler/list_crawler.py
@@ -1,9 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
 
-# 웹 페이지 URL
-url = "https://www.musinsa.com/categories/item/004"
-
 
 def get_goods_url_from_list_page(list_page_url):
     # GET 요청으로 웹 페이지의 HTML 콘텐츠 받아오기
@@ -24,5 +21,8 @@ def get_goods_url_from_list_page(list_page_url):
         print("Error:", response.status_code)
 
 
-for i in get_goods_url_from_list_page(url):
-    print(i)
+for i in range(1, 22):  # 무신사 사이트의 대분류는 1~21
+    category_id = f"0{i}" if i > 10 else f"00{i}"
+    url = f"https://www.musinsa.com/categories/item/{category_id}"
+    for j in get_goods_url_from_list_page(url):
+        print(j)
