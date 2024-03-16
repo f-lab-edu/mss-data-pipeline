905f3aa4ac7f0d3c03c631cbbb1bccfb072bbffd
HumanIearning <kimdh3896@naver.com>
2024-03-16 18:04:55 +0900

    fix: delete lines used during test

--- crawler/save_to_dw.py
+++ crawler/save_to_dw.py
@@ -19,7 +19,7 @@ def create_insert_query(goods):
     return query
 
 
-for i in range(1, 2):  # 무신사 사이트의 대분류는 1~21
+for i in range(1, 21):  # 무신사 사이트의 대분류는 1~21
     category_id = f"0{i}" if i > 10 else f"00{i}"
     url = f"https://www.musinsa.com/categories/item/{category_id}"
     for goods_url in get_goods_url_from_list_page(url):
@@ -27,7 +27,6 @@ for i in range(1, 2):  # 무신사 사이트의 대분류는 1~21
         sql = create_insert_query(goods)
         print(sql)
         cursor.execute(sql)
-        break
 
 
 db.commit()
