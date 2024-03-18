c945fa0703bec88b6ae2f6c6389cd0ebb8cc5444
HumanIearning <kimdh3896@naver.com>
2024-03-18 18:47:23 +0900

    fix: when category id is 10, the last of url is 0010, but it should be 010 so fix that

--- crawler/save_to_dw.py
+++ crawler/save_to_dw.py
@@ -20,7 +20,7 @@ def create_insert_query(goods):
 
 
 for i in range(1, 21):  # 무신사 사이트의 대분류는 1~21
-    category_id = f"0{i}" if i > 10 else f"00{i}"
+    category_id = f"0{i}" if i >= 10 else f"00{i}"
     url = f"https://www.musinsa.com/categories/item/{category_id}"
     for goods_url in get_goods_url_from_list_page(url):
         goods = crawl_goods(goods_url)
