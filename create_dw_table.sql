984b0eb07ce78c323bdc39edd79be065b47e42a8
HumanIearning <kimdh3896@naver.com>
2024-03-22 14:50:04 +0900

    feat: sync sql with changed erd

--- create_dw_table.sql
+++ create_dw_table.sql
@@ -1,18 +1,13 @@
 -- postgres create table sql
 
-CREATE TABLE goods(
+CREATE TABLE immutable_goods_info(
 goods_id INTEGER PRIMARY KEY,
 name VARCHAR NOT NULL,
 main_thumbnail_url VARCHAR,
 regular_price INTEGER NOT NULL,
-sale_price INTEGER NOT NULL,
 category VARCHAR,
+sub_category VARCHAR,
 brand VARCHAR NOT NULL,
-view_in_recent_month INTEGER,
-sales_in_recent_year INTEGER,
-likes INTEGER,
-star_rating INTEGER,
-reviews INTEGER,
 created_at TIMESTAMP NOT NULL
 );
 
@@ -22,7 +17,20 @@ goods_id INTEGER ,
 content VARCHAR NOT NULL,
 main_thumbnail_url VARCHAR,
 likes INTEGER,
-create_at TIMESTAMP NOT NULL
+created_at TIMESTAMP NOT NULL
 );
 
-ALTER TABLE review ADD CONSTRAINT review_good_id_fk FOREIGN KEY (goods_id) REFERENCES goods(goods_id);
+CREATE TABLE mutable_goods_info(
+    mutable_goods_info_id INTEGER PRIMARY KEY,
+    goods_id INTEGER,
+    sale_price INTEGER NOT NULL,
+    view_in_recent_month INTEGER,
+    sales_in_recent_year INTEGER,
+    likes INTEGER,
+    star_rating INTEGER,
+    reviews INTEGER,
+    create_at TIMESTAMP NOT NULL
+)
+
+ALTER TABLE review ADD CONSTRAINT review_goods_id_fk FOREIGN KEY (goods_id) REFERENCES immutable_goods_info(goods_id);
+ALTER TABLE mutable_goods_info ADD CONSTRAINT mutable_goods_info_goods_id_fk FOREIGN KEY (goods_id) REFERENCES immutable_goods_info(goods_id);
