0018ffda7891caeb0e13b0d8b43da8291e3b3193
HumanIearning <kimdh3896@naver.com>
2024-04-18 19:02:44 +0900

    feat: upsert into dw rather than append

--- etl/review_word_frequency.py
+++ etl/review_word_frequency.py
@@ -1,10 +1,12 @@
 from dotenv import load_dotenv
 from os import getenv
 
-from pyspark.ml.feature import Bucketizer
-from pyspark.sql.functions import col, date_trunc, explode, split, count
+import psycopg2
+from psycopg2.extras import execute_values
 
-from create_sparksess import start_spark
+from pyspark.sql.functions import explode, split, count
+
+from etl.create_sparksess import start_spark
 
 
 def extract_data(spark, table_name):
@@ -29,24 +31,49 @@ def transform_data(df):
     words = df.select(
         "goods_id", explode(split("content", "\\s+")).alias("word")
     ).filter("word is not null and content is not null")
-    word_counts = words.groupBy("goods_id", "word").agg(count("*").alias("count"))
+    word_counts = words.groupBy("goods_id", "word").agg(count("*").alias("word_count"))
 
     return word_counts
 
 
-def load_data(df):
+def load_data(rows):
     load_dotenv()
-    df.write.format("jdbc").option(
-        "url", f"jdbc:postgresql://{getenv('dw_host')}/{getenv('dw_dbname')}"
-    ).option("driver", "org.postgresql.Driver").option(
-        "dbtable", "review_word_frequency"
-    ).option(
-        "user", f"{getenv('dw_user')}"
-    ).option(
-        "password", f"{getenv('dw_password')}"
-    ).mode(
-        "append"
-    ).save()
+    conn = psycopg2.connect(
+        host=getenv("dw_host"),
+        dbname=getenv("dw_dbname"),
+        user=getenv("dw_user"),
+        password=getenv("dw_password"),
+        port=getenv("dw_port"),
+    )
+
+    try:
+        cursor = conn.cursor()
+        sql = """
+                INSERT INTO review_word_frequency (goods_id, word, count)
+                VALUES %s
+                ON CONFLICT (goods_id, word) 
+                DO UPDATE SET 
+                    count = EXCLUDED.count + review_word_frequency.count;
+                """
+        batch_size = 100
+        batch = []
+
+        for row in rows:
+            batch.append((row.goods_id, row.word, row.word_count))
+            if len(batch) >= batch_size:
+                execute_values(cursor, sql, batch)
+                conn.commit()
+                batch = []
+
+        if batch:
+            execute_values(cursor, sql, batch)
+            conn.commit()
+
+    except psycopg2.Error as e:
+        print(f"Error: {e}")
+    finally:
+        cursor.close()
+        conn.close()
 
 
 def main():
@@ -54,7 +81,7 @@ def main():
     review = extract_data(spark, "review")
 
     df = transform_data(review)
-    load_data(df)
+    df.foreachPartition(load_data)
 
 
 if __name__ == "__main__":
