35b8ad596085d8a1c86d53b6179ef047053bf83e
HumanIearning <kimdh3896@naver.com>
2024-04-18 18:59:04 +0900

    feat: upsert into dw instead of append

--- etl/sale_by_price_and_category.py
+++ etl/sale_by_price_and_category.py
@@ -1,8 +1,11 @@
 from dotenv import load_dotenv
 from os import getenv
 
+import psycopg2
+from psycopg2.extras import execute_values
+
 from pyspark.ml.feature import Bucketizer
-from pyspark.sql.functions import col, date_trunc
+from pyspark.sql.functions import col
 
 from create_sparksess import start_spark
 
@@ -36,23 +39,50 @@ def transform_data(df):
     df_with_bins = df_with_bins.withColumn(
         "price_range", col("price_range").cast("int")
     )
+    sales_by_price_and_category = df_with_bins.drop("sale_price")
 
-    return df_with_bins
+    return sales_by_price_and_category
 
 
-def load_data(df):
+def load_data(rows):
     load_dotenv()
-    df.write.format("jdbc").option(
-        "url", f"jdbc:postgresql://{getenv('dw_host')}/{getenv('dw_dbname')}"
-    ).option("driver", "org.postgresql.Driver").option(
-        "dbtable", "sales_by_price_and_category"
-    ).option(
-        "user", f"{getenv('dw_user')}"
-    ).option(
-        "password", f"{getenv('dw_password')}"
-    ).mode(
-        "append"
-    ).save()
+    conn = psycopg2.connect(
+        host=getenv("dw_host"),
+        dbname=getenv("dw_dbname"),
+        user=getenv("dw_user"),
+        password=getenv("dw_password"),
+        port=getenv("dw_port"),
+    )
+
+    try:
+        cursor = conn.cursor()
+        sql = """
+            INSERT INTO sales_by_price_and_category (goods_id, category, sales, price_range)
+            VALUES %s
+            ON CONFLICT (goods_id, price_range) 
+            DO UPDATE SET 
+                category = EXCLUDED.category,
+                sales = EXCLUDED.sales;
+            """
+        batch_size = 100
+        batch = []
+
+        for row in rows:
+            batch.append((row.goods_id, row.category, row.sales, row.price_range))
+            if len(batch) >= batch_size:
+                execute_values(cursor, sql, batch)
+                conn.commit()
+                batch = []
+
+        if batch:
+            execute_values(cursor, sql, batch)
+            conn.commit()
+
+    except psycopg2.Error as e:
+        print(f"Error: {e}")
+    finally:
+        cursor.close()
+        conn.close()
 
 
 def main():
@@ -71,16 +101,16 @@ def main():
         .select(
             mut_trimmed["goods_id"],
             "category",
-            "regular_price",
             "sale_price",
-            "sales_in_recent_year",
-            date_trunc("hour", "created_at").alias("created_at"),
+            col("sales_in_recent_year").alias("sales"),
         )
-        .dropna(subset=["sales_in_recent_year"])
+        .dropna(subset=["sales"])
     )
 
     df = transform_data(goods_joined)
-    load_data(df)
+    distinct_df = df.drop_duplicates(["goods_id", "price_range"])
+    distinct_df.show()
+    distinct_df.foreachPartition(load_data)
 
 
 if __name__ == "__main__":
