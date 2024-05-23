1560519e0fdf4a16817460abc89c4056138f5af8
HumanIearning <kimdh3896@naver.com>
2024-05-23 21:03:22 +0900

    chore: prevent from run main script when imported

--- crawler/load_to_s3.py
+++ crawler/load_to_s3.py
@@ -39,31 +39,34 @@ def upload_to_s3(s3, file_name, s3_path):
         print(e)
 
 
-s3 = get_s3_connection()
-file_path = "temp.html"
-dt = datetime.now(timezone(timedelta(hours=9))).strftime("%Y-%m-%d")
-for i in range(1, 22):  # 무신사 사이트의 대분류는 1~21
-    category_id = f"0{i}" if i >= 10 else f"00{i}"
-    url = f"https://www.musinsa.com/categories/item/{category_id}"
-    for goods_url in get_goods_url_from_list_page(url):
-        print("start crawl goods")
-        goods_id = goods_url.split("/")[-1]
-        try:
-            goods_html = get_page_html_from_url(goods_url)
-        except Exception as e:
-            print(e, goods_url)
-            continue
-        soup = get_soup_object_from_html(goods_html)
-        with open(file_path, "w", encoding="utf-8") as file:
-            file.write(soup.prettify())
-        upload_to_s3(s3, file_path, f"product_detail/{dt}/{goods_id}.html")
-        print("start crawl review")
-        for review, page_num, review_category in get_goods_review(goods_id):
-            with open(file_path, "w", encoding="utf-8") as file:
-                file.write(review)
+if __name__ == "__main__":
+    s3 = get_s3_connection()
+    file_path = "temp.html"
+    dt = datetime.now(timezone(timedelta(hours=9))).strftime("%Y-%m-%d")
+    for i in range(1, 22):  # 무신사 사이트의 대분류는 1~21
+        category_id = f"0{i}" if i >= 10 else f"00{i}"
+        url = f"https://www.musinsa.com/categories/item/{category_id}"
+        for goods_url in get_goods_url_from_list_page(url):
+            print("start crawl goods")
+            goods_id = goods_url.split("/")[-1]
+            try:
+                goods_html = get_page_html_from_url(goods_url)
+            except Exception as e:
+                print(e, goods_url)
+                continue
 
-            upload_to_s3(
-                s3,
-                file_path,
-                f"product_review/{dt}/{goods_id}/{review_category}/{page_num}.html",
-            )
+            soup = get_soup_object_from_html(goods_html)
+
+            with open(file_path, "w", encoding="utf-8") as file:
+                file.write(soup.prettify())
+            upload_to_s3(s3, file_path, f"product_detail/{dt}/{goods_id}.html")
+            print("start crawl review")
+            for review, page_num, review_category in get_goods_review(goods_id):
+                with open(file_path, "w", encoding="utf-8") as file:
+                    file.write(review)
+
+                upload_to_s3(
+                    s3,
+                    file_path,
+                    f"product_review/{dt}/{goods_id}/{review_category}/{page_num}.html",
+                )
