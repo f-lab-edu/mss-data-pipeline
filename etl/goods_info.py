c384e6cd55d30def113835e9f1b3ab2e0689e5fc
HumanIearning <kimdh3896@naver.com>
2024-05-25 18:43:16 +0900

    feat: script that extract html from s3 then, process and save to rds

--- etl/goods_info.py
+++ etl/goods_info.py
@@ -1,15 +1,38 @@
+import os
+
+from crawler.load_to_rds import (
+    create_immutable_goods_info_insert_query,
+    create_mutable_goods_info_insert_query,
+)
+from util.date import KST_now
+from util.postgresql import manipulate_data
+from util.s3 import get_s3_connection
+from crawler.process_goods_html import process_goods_html
+
+
 s3 = get_s3_connection()
 response = s3.list_objects_v2(
-    Bucket=os.getenv("s3_bucket_name"), Prefix="product_review/2024-05-24/"
+    Bucket=os.getenv("s3_bucket_name"), Prefix=f"product_detail/{KST_now()}/"
 )
 if "Contents" in response:
     files = response["Contents"]
     for file in files:
         file_key = file["Key"]
-        print(f"Fetching file: {file_key}")
-        # 파일 가져오기
-        # obj = s3.get_object(Bucket=bucket_name, Key=file_key)
-        # file_content = obj["Body"].read().decode("utf-8")  # 필요한 경우 디코딩
-        # print(f"File content from {file_key}:\n{file_content}\n")
+        print(file_key)
+        obj = s3.get_object(Bucket=os.getenv("s3_bucket_name"), Key=file_key)
+        file_content = obj["Body"].read().decode("utf-8")
+        goods_info = process_goods_html(file_content)
+        goods_info["goods_id"] = file_key.split("/")[-1].split(".")[0]
+        immutable_goods_info_insert_query = create_immutable_goods_info_insert_query(
+            goods_info
+        )
+        mutable_goods_info_insert_query = create_mutable_goods_info_insert_query(
+            goods_info
+        )
+
+        manipulate_data(immutable_goods_info_insert_query)
+        manipulate_data(mutable_goods_info_insert_query)
+
+
 else:
     print("No files found")
