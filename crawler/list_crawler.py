a94094983bee9ec5bfa98bc83e5fac469dedf749
HumanIearning <kimdh3896@naver.com>
2024-06-11 20:55:59 +0900

    as musinsa site is changed, synchronize code

--- crawler/list_crawler.py
+++ crawler/list_crawler.py
@@ -1,16 +1,34 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 
 
 def get_goods_url_from_list_page(list_page_url):
-    response = requests.get(list_page_url)
+    response = get_page_html_from_url(list_page_url)
+    soup = BeautifulSoup(response, "lxml")
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, "lxml")
+    links = soup.find_all("a", {"class": "category__sc-rb2kzk-7 ksmIyr"})
 
-        links = soup.find_all("a", attrs={"name": "goods_link"})
+    for link in links:
+        yield link.get("href")
 
-        for link in links[::2]:
-            yield f"https:{link.get('href')}"
-    else:
-        print("Error:", response.status_code)
+
+def get_page_html_from_url(url):
+    user_agent = {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+    }
+    chrome_option = webdriver.ChromeOptions()
+    chrome_option.add_argument("--headless")
+    chrome_option.add_argument("--no-sandbox")
+    chrome_option.add_argument("--disable-dev-shm-usage")
+    chrome_option.add_argument(f'--user-agent={user_agent["User-Agent"]}')
+    chrome_option.add_argument("--disable-gpu")
+    browser = webdriver.Chrome(options=chrome_option)
+    browser.get(url)
+
+    html = browser.page_source
+    browser.quit()
+
+    return html
