9125a22d413fded5ac0628001ecb43fb3fe66ed3
HumanIearning <kimdh3896@naver.com>
2024-06-11 20:56:41 +0900

    remove unused statement

--- crawler/list_crawler.py
+++ crawler/list_crawler.py
@@ -1,7 +1,3 @@
-import re
-
-import requests
-from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
