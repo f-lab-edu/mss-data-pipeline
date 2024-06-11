e6df9f909b9ea88deaa9d0fbb5d99c11ec04c58f
HumanIearning <kimdh3896@naver.com>
2024-06-11 20:56:12 +0900

    as musinsa site is changed, synchronize code

--- crawler/list_crawler.py
+++ crawler/list_crawler.py
@@ -3,11 +3,16 @@ import re
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from crawler.crawl_musinsa import get_soup_object_from_html
 
 
 def get_goods_url_from_list_page(list_page_url):
     response = get_page_html_from_url(list_page_url)
-    soup = BeautifulSoup(response, "lxml")
+    soup = get_soup_object_from_html(response)
 
     links = soup.find_all("a", {"class": "category__sc-rb2kzk-7 ksmIyr"})
 
@@ -27,6 +32,9 @@ def get_page_html_from_url(url):
     chrome_option.add_argument("--disable-gpu")
     browser = webdriver.Chrome(options=chrome_option)
     browser.get(url)
+    WebDriverWait(browser, 20).until(
+        EC.presence_of_element_located((By.CLASS_NAME, "category__sc-rb2kzk-7.ksmIyr"))
+    )
 
     html = browser.page_source
     browser.quit()
